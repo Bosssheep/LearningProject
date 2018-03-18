59eb6e78a7f526906c501b65ef68a5dabfcfae2a
ChenHY <389137297@qq.com>
2018-03-18 14:06:56 +0800

    a login web demo using Servlet

---
+++ ServletLoginDemo/src/servlet/LoginServlet.java
@@ -0,0 +1,59 @@
+package servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.po.Users;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doPost(request,response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		/*
+		 * 登陆验证
+		 */
+		
+		Users u = new Users();
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		u.setUsername(username);
+		u.setPassword(password);
+		
+		if(u.getUsername().equals("admin")&&u.getPassword().equals("admin"))
+		{
+			response.sendRedirect(request.getContextPath()+"/login_success.jsp");
+			
+		}else{
+			response.sendRedirect(request.getContextPath()+"/login_failure.jsp");
+		}
+		
+	}
+
+}
